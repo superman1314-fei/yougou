e873115a58606db9a05ba3a60bddde6d870275d8
Ahty_M <motejunzhuanyong@163.com>
2020-02-28 19:56:25 +0800

    菜单导航栏

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 //app.js
+import request from './utils/request.js'
 App({
-
+    onLaunch(){
+      //指定基准路径
+      request.defaults.baseURL="https://api-hmugo-web.itheima.net/api/public/v1"
+    }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,36 @@
+  //引入api
+  import request from '../../utils/request.js'
 Page({
- 
+  data:{
+    //背景图
+    banners: [],
+    //导航菜单栏
+    menus:[]
+},
+  onLoad() {
+    request({
+      url:"/home/swiperdata"
+    }).then(res=>{
+      const {message} = res.data
+      this.setData({
+        banners: message
+      })
+    })
+    //导航菜单栏请求数据
+    request({
+      url:'/home/catitems'
+    }).then(res=>{
+      const {message} = res.data
+      console.log(res)
+      const newData = message.map((v,i)=>{
+        if(i===0){
+          v.url="/pages/category/index"
+        }
+        return v
+      })
+      this.setData({
+        menus: newData
+      })
+    })
+  }
 })
