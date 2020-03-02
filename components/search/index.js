0f2f76fc15a55722813f343f3c50f43b4165de6e
Ahty_M <motejunzhuanyong@163.com>
2020-03-02 13:02:20 +0800

    优化分类功能

--- components/search/index.js
+++ components/search/index.js
@@ -1,4 +1,5 @@
 // components/search/index.js
+
 Component({
   /**
    * 组件的属性列表
--- pages/category/index.js
+++ pages/category/index.js
@@ -1,33 +1,45 @@
-import request from '../../utils/request.js'
+import request from "../../utils/request.js"
+
+// pages/category/index.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    //数据列表
-    list:[],
-    content:0//索引
+    // 记录当前点击了哪个菜单
+    current: 0,
+    // 右侧回到顶部
+    scrollTop:0,
+    // 数据列表
+    list: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-      request({
-        url:"/categories"
-      }).then(res=>{
-        const {message} =res.data
-        this.setData({
-          list: message
-        })
-      })
-  },
-  handelClick(e){
-    const { index } = e.currentTarget.dataset
+    // 请求分类页的数据
+    request({
+      url: "/categories"
+    }).then(res => {
+      // 把数据列表保存到data
+      const { message } = res.data;
       this.setData({
-        content: index
+        list: message
       })
-  }
+    })
+  },
 
+  // 点击切换左边菜单栏时候触发
+  handleClick(e) {
+    // index参数
+    const { index } = e.currentTarget.dataset;
+
+    // 保存当前点击的索引到data
+    this.setData({
+      current: index,
+      scrollTop:0
+    })
+  }
 })
