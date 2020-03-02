8ba8491bf921e42b690f56c375d2f55a5304b4af
Ahty_M <motejunzhuanyong@163.com>
2020-03-02 20:29:45 +0800

    实现功能/防抖/加载数据/分页

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -8,7 +8,13 @@ Page({
       //关键字
     keyword:"",
     // 列表内容
-    goods:[]
+    goods:[],
+    //是否没有数据
+    showMore:true,
+    //页码
+    pagenum:1,
+    //开关/防抖
+    loding:true
   },
 
   /**
@@ -20,28 +26,52 @@ Page({
     this.setData({
       keyword
     });
-    //发起请求
-    request({
-      url:'/goods/search',
-      data:{
-        query:this.data.keyword,
-        pagenum:1,
-        pagesize:10
-      }
-    }).then(res=>{
-      const{message} = res.data
-      //价格后面加两个小数点
-      const goods = message.goods.map(v=>{
-        v.goods_price=Number(v.goods_price).toFixed(2)
-        return v
-      })
-      this.setData({
-        goods
-      })
-      
-    })
+    this.getGoods()
 
-  }  
+  },
+  getGoods(){
+    //发起请求
+    if(this.data.showMore===false) return
+    setTimeout(v=>{
+      request({
+        url: '/goods/search',
+        data: {
+          query: this.data.keyword,
+          pagenum: this.data.pagenum,
+          pagesize: 10
+        }
+      }).then(res => {
+        const { message } = res.data
+        //价格后面加两个小数点
+        const goods = message.goods.map(v => {
+          v.goods_price = Number(v.goods_price).toFixed(2)
+          return v
+        })
+
+        this.setData({
+          goods:[...this.data.goods,...goods],
+          loding:false
+        });
+        if(this.data.goods.length>=message.total){
+          this.setData({
+            showMore:false
+          })
+        }
+        })
+    },2000)
+    
+
+   
+  },
+onReachBottom(){
+  if (this.data.loding===false){
+    this.setData({
+      pagenum: this.data.pagenum + 1,
+      loding:true
+    })
+    this.getGoods()
+  }
+  }
 
 
 })
