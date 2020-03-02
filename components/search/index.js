d621babf31ae40427d6c7c37a0f3f48b67594af9
Ahty_M <motejunzhuanyong@163.com>
2020-03-02 23:24:28 +0800

    自定义组件+回到顶部

--- components/search/index.js
+++ components/search/index.js
@@ -4,10 +4,19 @@ Component({
   /**
    * 组件的属性列表
    */
+  //组件传值相当于props
   properties: {
-
+      keyword:{
+        type:String,
+        value:'搜索' //默认
+      },
+    align:{
+      type:String,
+      value: 'center' //left center right 样式
+    }
   },
-
+  // 外部样式
+  externalClasses: ['background'],
   /**
    * 组件的初始数据
    */
--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,4 +1,4 @@
-import request from '../../utils/request.js'
+import request from '../../utils/request.js' //请求数据的方法
 Page({
 
   /**
@@ -14,7 +14,9 @@ Page({
     //页码
     pagenum:1,
     //开关/防抖
-    loding:true
+    loding:true,
+    //回到顶部
+    isShowTop: false
   },
 
   /**
@@ -26,19 +28,21 @@ Page({
     this.setData({
       keyword
     });
+    //调用数据
     this.getGoods()
 
   },
   getGoods(){
     //发起请求
+    // 如果showMore没有数据 就不要请求了
     if(this.data.showMore===false) return
-    setTimeout(v=>{
+    
       request({
         url: '/goods/search',
         data: {
-          query: this.data.keyword,
-          pagenum: this.data.pagenum,
-          pagesize: 10
+          query: this.data.keyword,//关键词
+          pagenum: this.data.pagenum, //页数
+          pagesize: 10 //码数
         }
       }).then(res => {
         const { message } = res.data
@@ -49,29 +53,54 @@ Page({
         })
 
         this.setData({
+          //把旧数组和新数组添加到同一个
           goods:[...this.data.goods,...goods],
           loding:false
         });
+        //是否还在加载中
         if(this.data.goods.length>=message.total){
           this.setData({
             showMore:false
           })
         }
         })
-    },2000)
-    
-
-   
   },
+  //小程序回到顶部
+  headTop() {
+    wx.pageScrollTo({
+      scrollTop: 0,
+      duration: 300
+    })
+  },
+
 onReachBottom(){
+  // 判断开关 请求上一次成功后 再请求后面的数据
   if (this.data.loding===false){
+    // 每次加载页数加一
     this.setData({
       pagenum: this.data.pagenum + 1,
-      loding:true
+      loding:true //开关
     })
-    this.getGoods()
+    this.getGoods() //调用请求
   }
+  },
+  //监听回到顶部
+  onPageScroll(e) {
+    const { scrollTop } = e
+    //当前状态
+    let isShow = this.data.isShowTop
+    //大于100就显示回到顶部
+    if (scrollTop > 100) {
+      isShow = true
+    } else {
+      isShow = false
+    }
+    //判断是否相同
+    if (isShow == this.data.isShowTop) return
+    //赋值
+    this.setData({
+      isShowTop: isShow
+    })
   }
-
 
 })
