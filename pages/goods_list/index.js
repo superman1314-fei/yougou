e098d44ee076b48b1532ada9cff5c51c65bfc9e7
Ahty_M <motejunzhuanyong@163.com>
2020-03-02 19:29:32 +0800

    渲染数据

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,4 +1,4 @@
-// pages/goods_list/index.js
+import request from '../../utils/request.js'
 Page({
 
   /**
@@ -6,18 +6,42 @@ Page({
    */
   data: {
       //关键字
-    keyword:""
+    keyword:"",
+    // 列表内容
+    goods:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    //拿到关键词
     const {keyword} = options
     this.setData({
       keyword
+    });
+    //发起请求
+    request({
+      url:'/goods/search',
+      data:{
+        query:this.data.keyword,
+        pagenum:1,
+        pagesize:10
+      }
+    }).then(res=>{
+      const{message} = res.data
+      //价格后面加两个小数点
+      const goods = message.goods.map(v=>{
+        v.goods_price=Number(v.goods_price).toFixed(2)
+        return v
+      })
+      this.setData({
+        goods
+      })
+      
     })
-  },  
+
+  }  
 
 
 })
