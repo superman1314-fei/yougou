d023a05263b69eb4b30a98daeae441054785f3bf
Ahty_M <motejunzhuanyong@163.com>
2020-03-04 00:09:32 +0800

    添加本地存储功能 实现历史记录保存

--- pages/search/index.js
+++ pages/search/index.js
@@ -12,20 +12,29 @@
      //搜索建议
      recommend: [],
      //节流 /开关
-     loading: false
+     loading: false,
+     //本地存储
+     localKey:[]
    },
 
    /**
     * 生命周期函数--监听页面加载
     */
    onLoad: function(options) {
-
+     //获取本地数据
+     let arr = wx.getStorageSync('localKey')
+    //  判断arr是否是数据 如果不是就让它等于一个数组
+          if(!Array.isArray(arr)){
+            arr=[]
+          }
+          //赋值
+        this.setData({
+          localKey:arr
+        })
    },
    // 是获取input的数据
    valueInput(e) {
-     const {
-       value
-     } = e.detail
+     const {value} = e.detail
      this.setData({
        inputValue: value
      });
@@ -77,5 +86,23 @@
      }
   
 
+   },
+   //按下回车键跳转列表页
+   handelConfirm(){
+    //  let arr = wx.getStorageSync('localKey')
+    //  if(!Array.isArray(arr)){
+    //    arr=[]
+    //  }
+   
+     //本地存储
+     let arr = this.data.localKey
+     arr.unshift(this.data.inputValue) //把数据添加到前面
+     //数组去重
+     arr = [...new Set(arr)]
+     wx.setStorageSync('localKey', arr)
+     wx.redirectTo({
+       url: "/pages/goods_list/index?keyword=" + this.data.inputValue
+     })
    }
+   
  })
