77c0599deb5510906f8b8de881ae5ee6c2b2ce94
Ahty_M <motejunzhuanyong@163.com>
2020-03-04 22:34:13 +0800

    客服会话

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,17 +1,38 @@
-// pages/goods_detail/index.js
+import request from "../../utils/request.js"
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    banners: ["https://img11.360buyimg.com/pop/s1180x940_jfs/t1/103557/6/14052/88126/5e5efa66E8b8e2594/71b285b0414267a8.jpg","https://img20.360buyimg.com/pop/s1180x940_jfs/t1/85749/26/13362/98247/5e579c3eE3e78c862/7c91d105322871af.jpg"]
+  //商品内容
+    detail:[],
+    //索引
+    current:0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    const { id } = options
+     request({
+       url:'/goods/detail',
+       data:{
+         goods_id:id
+       }
+     }).then(res=>{
+      //  console.log(res)
+       const { message} = res.data
+       this.setData({
+         detail:message
+       })
+     })
+  },
+  handelTap(e){
+    const { index } = e.currentTarget.dataset
+        this.setData({
+          current : index
+        })
   }
 })
