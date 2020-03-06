ea3d91770e0265229f450f0b541f34063604acc8
Ahty_M <motejunzhuanyong@163.com>
2020-03-07 00:07:17 +0800

    实现加一 减一 总价格计算/图片跳转

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -48,14 +48,55 @@ Page({
       }
     })
   },
+  //计算总价格
   handleAllPrice(){
+    //定义一个价格为0
       let price = 0
+      // 循环找打number 让他加乘价格
       this.data.goods.forEach(v=>{
         price+= v.goods_price * v.number
       })
+    //修改数据
       this.setData({
         allPrice: price
       })
+      //重新赋值给本地存储
+    wx.setStorageSync("goods", this.data.goods)
+  },
+  //数量的加减
+  handleCalc(e){
+    //结构出来
+    const {index,number} = e.currentTarget.dataset
+      // 让number 加上1 页面不会刷新
+    this.data.goods[index].number += number
+    //判断如果number = 0 就删除改商品
+    if (this.data.goods[index].number===0){
+      wx.showModal({
+        title: '提示',
+        content: '确定要删除吗',
+        success:(res)=> {
+          if (res.confirm) {
+            //删除
+            this.data.goods.splice(index,1)
+          }else{
+            //如果按了取消按钮 给number加上1
+            this.data.goods[index].number+=1
+            //重新计算总价格
+            this.handleAllPrice()
+          }
+          //刷新页面更改goods值
+          this.setData({
+            goods: this.data.goods
+          })
+        }
+      })
+    }
+    //刷新页面
+    this.setData({
+      goods: this.data.goods
+    })
+    //调用计算总价格
+    this.handleAllPrice()
   }
 
 })
