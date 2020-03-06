ec4a4db146fdb457ae3671b80211d72688034459
Ahty_M <motejunzhuanyong@163.com>
2020-03-06 21:53:26 +0800

    将收货地址保存到本地

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -5,62 +5,37 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+      //获取地址信息
+      address:{}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({
+      address: wx.getStorageSync("address") || {}
+    })
+    
   },
+  handelGetAddress(){
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    wx.chooseAddress({
+      success:(res) =>{
+        this.setData({
+          address:{
+            //姓名
+            name: res.userName,
+            // 手机号
+            tel: res.telNumber,
+            //地址
+            detall: res.provinceName + res.cityName + res.countyName + res.detailInfo
+          }
+        });
+        //保存地址信息
+        wx.setStorageSync("address", this.data.address)
+      }
+    })
   }
+
 })
