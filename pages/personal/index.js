e79eaff12be8c575ac4d0203cbd1c445ca534065
Ahty_M <motejunzhuanyong@163.com>
2020-03-11 00:59:00 +0800

    添加一些方法

--- pages/personal/index.js
+++ pages/personal/index.js
@@ -22,52 +22,14 @@ Page({
       })
     }
   },
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  //收获地址
+  handelArss(){
+    wx.chooseAddress()
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //客服
+  handelCall(){
+    wx.makePhoneCall({
+      phoneNumber: '13826707429' //仅为示例，并非真实的电话号码
+    })
   }
 })
